f4284f80771b7c38dc0c7a4b501854bfcd1635fb
massagehero <onyx18121990@gmail.com>
2020-10-30 11:26:31 +0200

    added new style for comming soon page

--- wp-content/themes/pal-theme/templates/page-soon.php
+++ wp-content/themes/pal-theme/templates/page-soon.php
@@ -5,27 +5,27 @@
 get_header(); ?>
 
 <div>
-	<img src="<?php echo IMG.'/logo.svg'; ?>" alt="" class="logo">
-	<h1>Coming soon...</h1>
+  <img src="<?php echo IMG.'/logo.png'; ?>" alt="" class="logo">
+  <h1>Coming soon...<br>OPENING on November 10</h1>
 </div>
 
 <style>
-	body{
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: 100vh;
-		margin: 0;
-		padding: 0;
-	}
-	.main-header, .main-footer{
-		display: none;
-	}
-	.logo{
-		position: fixed;
-		top: 30px; left: 50%;
-		transform: translate(-50%, 0);
-	}
+  body{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+  }
+  .main-header, .main-footer{
+    display: none;
+  }
+  .logo{
+    position: fixed;
+    top: 30px; left: 50%;
+    transform: translate(-50%, 0);
+  }
 </style>
 
 <?php get_footer();
