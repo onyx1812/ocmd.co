9c3dec7a7d6cc75af292add89799673553840fdd
massagehero <onyx18121990@gmail.com>
2020-12-23 16:47:02 +0200

    added

--- wp-content/themes/pal-theme/single.php
+++ wp-content/themes/pal-theme/single.php
@@ -2,9 +2,30 @@
 
   <?php while ( have_posts() ) : the_post(); ?>
     <div class="container">
-      <h1 class="section-title"><?php the_title(); ?></h1>
-      <div class="content"><?php the_content(); ?></div>
+      <div class="single-content">
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+      </div>
     </div>
+<style>
+.single-content {
+  padding: 60px 0;
+}
+.single-content h1 {
+  font-size: 50px;
+  font-weight: 700;
+  margin-bottom: 30px;
+}
+.single-content p, .single-content li {
+  font-size: 18px;
+  color: #1f1f1f;
+  margin-bottom: 15px;
+}
+.single-content ol {
+  list-style: decimal outside;
+  padding: 10px 30px;
+}
+</style>
   <?php endwhile; ?>
 
 <?php get_footer(); ?>
