a83824f1aba8984c68f01aa7d08773edbb6c4b54
massagehero <onyx18121990@gmail.com>
2020-12-23 16:40:23 +0200

    added single page

--- wp-content/themes/pal-theme/single.php
+++ wp-content/themes/pal-theme/single.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
-		<div class="container">
-			<?php the_content(); ?>
-		</div>
-	<?php endwhile; ?>
+  <?php while ( have_posts() ) : the_post(); ?>
+    <div class="container">
+      <h1 class="section-title"><?php the_title(); ?></h1>
+      <div class="content"><?php the_content(); ?></div>
+    </div>
+  <?php endwhile; ?>
 
 <?php get_footer(); ?>
