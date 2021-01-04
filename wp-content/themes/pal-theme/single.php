60682fea5022d6269695a5e33b7d89813af01f3f
massagehero <onyx18121990@gmail.com>
2021-01-04 08:44:00 +0200

    add images floats

--- wp-content/themes/pal-theme/single.php
+++ wp-content/themes/pal-theme/single.php
@@ -4,7 +4,9 @@
     <div class="container">
       <div class="single-content">
         <h1><?php the_title(); ?></h1>
-        <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="post-img" width="50%">
+        <?php if( get_the_post_thumbnail_url() ): ?>
+          <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="post-img" width="50%">
+        <?php endif; ?>
         <?php the_content(); ?>
         <p class="text-center"><a class="link" title="Home page OCMD" href="<?php echo home_url(); ?>">< Go to Home page</a></p>
       </div>
