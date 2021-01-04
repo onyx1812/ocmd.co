32077764096bf0ed5e45d403a75d6e9bb6f8fc26
massagehero <onyx18121990@gmail.com>
2021-01-04 08:33:53 +0200

    added post image

--- wp-content/themes/pal-theme/single.php
+++ wp-content/themes/pal-theme/single.php
@@ -4,6 +4,7 @@
     <div class="container">
       <div class="single-content">
         <h1><?php the_title(); ?></h1>
+        <p><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt=""></p>
         <?php the_content(); ?>
         <p class="text-center"><a class="link" title="Home page OCMD" href="<?php echo home_url(); ?>">< Go to Home page</a></p>
       </div>
