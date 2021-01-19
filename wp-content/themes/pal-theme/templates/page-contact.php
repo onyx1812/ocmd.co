0ebc65e85e2bbbb82f05cffb87d281328c4a1800
massagehero <onyx18121990@gmail.com>
2021-01-19 15:36:44 +0200

    added

--- wp-content/themes/pal-theme/templates/page-contact.php
+++ wp-content/themes/pal-theme/templates/page-contact.php
@@ -7,71 +7,21 @@ while ( have_posts() ) : the_post();
 
 
 
-    // $email = 'max@purpleadlab.com';
-    // $name = 'Max';
-    // $message = 'Hello world';
+    $email = 'max@purpleadlab.com';
+    $name = 'Max';
+    $message = 'Hello world';
 
-    // $to = "onyx18121990@gmail.com";
-    // $subject = "Your order has been sent";
-    // $text =  $message;
+    $to = "onyx18121990@gmail.com";
+    $subject = "Your order has been sent";
+    $text =  $message;
 
-    // $headers = 'From: Thezense team <webmaster@ocmd.co>' . "\r\n" .
-    // 'Reply-To: webmaster@ocmd.co' . "\r\n" .
-    // 'X-Mailer: PHP/' . phpversion();
+    $headers = 'From: Thezense team <webmaster@ocmd.co>' . "\r\n" .
+    'Reply-To: webmaster@ocmd.co' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-    // $sending = mail($to, $subject, $text, $headers);
+    $sending = mail($to, $subject, $text, $headers);
 
-    // if($sending) echo "Email sent!";
-
-
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'vendor/autoload.php';
-
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-    //Server settings
-    $mail->setLanguage('ru', 'vendor/phpmailer/phpmailer/language/'); // Перевод на русский язык
-
-    //Enable SMTP debugging
-    // 0 = off (for production use)
-    // 1 = client messages
-    // 2 = client and server messages
-    $mail->SMTPDebug = 1;                                 // Enable verbose debug output
-
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-
-    //$mail->SMTPSecure = 'ssl';                          // secure transfer enabled REQUIRED for Gmail
-    //$mail->Port = 465;                                  // TCP port to connect to
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
-
-    $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
-    $mail->Username = 'onyx18121990@gmail.com';               // SMTP username
-    $mail->Password = '9J8iak45@@';                         // SMTP password
-
-    //Recipients
-    $mail->setFrom('webmaster@ocmd.co', 'webmaster');
-    $mail->addAddress('webmaster@ocmd.co');              // Name is optional
-
-    //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Test mail to user';
-    $mail->Body    = 'This is the very simple HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-}
-
-
+    if($sending) echo "Email sent!";
 
   ?>
 
