b4e6926fdf38f182f8bde0343e20fe1e9717567e
massagehero <onyx18121990@gmail.com>
2021-01-23 10:56:32 +0200

    added js

--- wp-content/themes/pal-theme/include/subscribeForm.php
+++ wp-content/themes/pal-theme/include/subscribeForm.php
@@ -1,6 +1,8 @@
 <?php
 
 function subscribeForm(){
+
+	/*Send email to support*/
 	$email = $_POST['email'];
 
 	$to = "support@ocmd.co";
@@ -9,7 +11,7 @@ function subscribeForm(){
 	$text = 'email: '. $email;
 
 	$headers = 'From: Subscribe Form <admin@ocmd.co>' . "\r\n" .
-	'Reply-To: $email' . "\r\n" .
+	'Reply-To: '.$email. "\r\n" .
 	'X-Mailer: PHP/' . phpversion();
 
 	$sending = wp_mail($to, $subject, $text, $headers);
@@ -19,6 +21,25 @@ function subscribeForm(){
 	} else {
 		echo 'error';
 	}
+
+	// /*Send email to sibscriber*/
+	// $email = $_POST['email'];
+
+	// $to = $email;
+	// $subject = 'Subscribing from subscribe form ocmd.co';
+	// $text = 'email: '. $email;
+
+	// $headers = 'From: Subscribe Form <admin@ocmd.co>' . "\r\n" .
+	// 'Reply-To: '.$email.'' . "\r\n" .
+	// 'X-Mailer: PHP/' . phpversion();
+
+	// $sending = wp_mail($to, $subject, $text, $headers);
+
+	// if($sending){
+	// 	echo 'sucess';
+	// } else {
+	// 	echo 'error';
+	// }
 
 	wp_die();
 }
