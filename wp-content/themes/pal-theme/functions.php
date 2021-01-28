63e3115c255bf7b0543e121103e3339972ba2a90
massagehero <onyx18121990@gmail.com>
2021-01-28 13:35:28 +0200

    added changes

--- wp-content/themes/pal-theme/functions.php
+++ wp-content/themes/pal-theme/functions.php
@@ -14,6 +14,8 @@ function TYPE(){
   return $post->post_type;
 }
 
+TYPE();
+
 function replace_core_jquery_version() {
   wp_deregister_script( 'jquery' );
   wp_register_script( 'jquery', "https://code.jquery.com/jquery-3.1.1.min.js", array(), '3.1.1' );
