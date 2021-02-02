1bb6a66db4212d4407c2a2715e4cb10bc57e3de0
massagehero <onyx18121990@gmail.com>
2021-02-02 16:19:57 +0200

    added class

--- wp-content/themes/pal-theme/page.php
+++ wp-content/themes/pal-theme/page.php
@@ -6,10 +6,12 @@ get_header();
 while ( have_posts() ) : the_post(); ?>
 
 <section class="page-default">
-	<div class="container">
-		<h1 class="section-title"><?php the_title(); ?></h1>
-		<?php the_content(); ?>
-	</div>
+  <div class="container">
+    <?php if( !is_account_page() ): ?>
+    <h1 class="section-title"><?php the_title(); ?></h1>
+    <?php endif; ?>
+    <?php the_content(); ?>
+  </div>
 </section>
 
 <?php
