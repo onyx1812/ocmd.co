b36726344d5677b6b89371f3a7227c6a9eddfcdf
massagehero <onyx18121990@gmail.com>
2021-03-02 10:43:33 +0200

    changes

--- wp-content/themes/pal-theme/header.php
+++ wp-content/themes/pal-theme/header.php
@@ -17,7 +17,6 @@
 
     <script>
       const url = window.location.href;
-      console.log(url);
       if( url == 'http://ocmd.co/shop' || url == 'https://ocmd.co/shop' || url == 'http://www.ocmd.co/shop' || url == 'https://ocmd.co/shop/' ){
         window.location.href = 'https://ocmd.co/#productsSection';
       } else if( url == 'http://ocmd.loc/shop' ){
